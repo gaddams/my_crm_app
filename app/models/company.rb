c76ca815043892986ad2d8282fd58cf65d0dc963
gaddams <sgaddam2015@kellogg.northwestern.edu>
2014-05-31 23:27:41 -0500

    changes before db:drop

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,6 @@ class Company < ActiveRecord::Base
 	belongs_to :user
 	has_many :contacts
 	has_many :interactions
+
+	validates :user_id, :presence => true
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
 	belongs_to :company
 	has_many :interactions
+
+	validates :company_id, :presence => true
 end
